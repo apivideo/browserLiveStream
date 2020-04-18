080b3a9141129b5b5ff4207acd966a9e5f60533e
Doug S <doug.sillars@gmail.com>
2020-04-18 17:30:41 +0200

    major ffmpeg perf issues

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ io.on('connection', function(socket){
 	socket.emit('message','Hello from mediarecorder-to-rtmp server!');
 	socket.emit('message','Please set rtmp destination before start streaming.');
 	
+	
+	
 	var ffmpeg_process, feedStream=false;
 	socket.on('config_rtmpDestination',function(m){
 		if(typeof m != 'string'){
@@ -67,11 +69,52 @@ io.on('connection', function(socket){
 			return;
 		}
 		
-		var ops=[
+		var framerate = socket.handshake.query.framespersecond;
+		var audioBitrate = parseInt(socket.handshake.query.audioBitrate);
+	    var audioEncoding = "64k";
+		if (audioBitrate ==11025){
+			audioEncoding = "11k";
+		} else if (audioBitrate ==22050){
+			audioEncoding = "22k";
+		} else if (audioBitrate ==44100){
+			audioEncoding = "44k";
+		}
+		console.log(audioEncoding, audioBitrate);
+		console.log('framerate on node side', framerate);
+		//var ops = [];
+		if (framerate == 1){
+			var ops = [
+				'-i','-',
+				 '-c:v', 'libx264', '-preset', 'ultrafast', '-tune', 'zerolatency', 
+				//'-max_muxing_queue_size', '1000', 
+				'-bufsize', '5000',
+			       '-r', '1', '-g', '2', '-keyint_min','2', 
+					'-x264opts', 'keyint=2', '-crf', '25', '-pix_fmt', 'yuv420p',
+			        '-profile:v', 'baseline', '-level', '3', 
+     				'-c:a', 'aac', '-b:a', audioEncoding, '-ar', audioBitrate, 
+			        '-f', 'flv', socket._rtmpDestination		
+			];
+			
+		}else if (framerate == 15){
+			var ops = [
+				'-i','-',
+				 '-c:v', 'libx264', '-preset', 'ultrafast', '-tune', 'zerolatency', 
+				'-max_muxing_queue_size', '1000', 
+				'-bufsize', '5000',
+			       '-r', '15', '-g', '30', '-keyint_min','30', 
+					'-x264opts', 'keyint=30', '-crf', '25', '-pix_fmt', 'yuv420p',
+			        '-profile:v', 'baseline', '-level', '3', 
+     				'-c:a', 'aac', '-b:a',audioEncoding, '-ar', audioBitrate, 
+			        '-f', 'flv', socket._rtmpDestination		
+			];
+			
+		}else{
+		   var ops=[
 			 '-i','-',
 			//'-c', 'copy', 
 			'-c:v', 'libx264', '-preset', 'ultrafast', '-tune', 'zerolatency',  // video codec config: low latency, adaptive bitrate
-			'-c:a', 'aac',// '-ar', '44100', '-b:a', '64k', // audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
+			'-c:a', 'aac', '-ar', audioBitrate, '-b:a', audioEncoding, // audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
+			//'-max_muxing_queue_size', '4000', 
 			//'-y', //force to overwrite
 			//'-use_wallclock_as_timestamps', '1', // used for audio sync
 			//'-async', '1', // used for audio sync
@@ -94,7 +137,8 @@ io.on('connection', function(socket){
 			*/
 			
 		];
-		
+	}
+	console.log("ops", ops);
 		console.log(socket._rtmpDestination);
 		ffmpeg_process=spawn('ffmpeg', ops);
 		console.log("ffmpeg spawned");
